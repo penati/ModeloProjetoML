effca3bf8d3a2811828046a7b2b819b4ff787624
Penati <filipepf15@gmail.com>
2018-06-25 02:04:54 -0300

    Reorganizado as sessões pois estou usando o summarise para gerar um gráfico

--- scripts/analise-exploratoria-de-dados.R
+++ scripts/analise-exploratoria-de-dados.R
@@ -35,53 +35,6 @@ head(time_21)
 tail(time_21)
 
 
-## MAKE FIGURES ####
-
-data.plot = ggplot(data, aes(x=Time, y=weight)) +
-  geom_point(aes(color=Diet), position = position_jitter(width = 0.2)) +
-  labs(title="Evolução do peso dos pintinhos nos 21º dias", x="Dias de vida", y="Peso (g)") +
-  theme_classic() +
-  theme(text=element_text(size=18), title=element_text(size=18))
-data.plot
-
-# Write figure to a pdf in the 'figures' folder
-pdf("figures/aumento-peso-pintinhos.pdf")
-# Call plot
-data.plot
-# Close pdf call
-dev.off()
-
-time_21.plot = ggplot(time_21, aes(x=Diet, y=weight)) +
-  geom_boxplot(aes(color=Diet)) +
-  geom_point(aes(color=Diet), position = position_jitter(width = 0.05)) +
-  labs(title="Peso dos pintinhos aos 21 dias de vida", x="Dieta", y="Peso (g)") +
-  theme_classic() +
-  theme(text=element_text(size=18), title=element_text(size=18), legend.position="none")
-time_21.plot
-
-# Write figure to a pdf in the 'figures' folder
-pdf("figures/peso-21dias-pintinhos.pdf")
-# Call plot
-time_21.plot
-# Close pdf call
-dev.off()
-
-weight_time_diet.plot = ggplot(weight_by_time_and_diet, aes(x=Time, y=weight_mean)) +
-  geom_smooth(aes(color=Diet), se=FALSE) +
-  geom_point(aes(color=Diet))+
-  labs(title="Evolução do peso dos pintinhos de acordo com a dieta", x="Dias de vida", y="Peso (g)") +
-  theme_classic() +
-  theme(text=element_text(size=18), title=element_text(size=18))
-weight_time_diet.plot
-
-
-# Write figure to a pdf in the 'figures' folder
-pdf("figures/media-dietas-pintinhos.pdf")
-# Call plot
-weight_time_diet.plot
-# Close pdf call
-dev.off()
-
 ## RUN DESCRIPTIVE STATISTICS ####
 ### Summarise data
 
@@ -117,3 +70,52 @@ weight_21days_by_diet <- time_21 %>%
             weight_max = max(weight))
 
 weight_21days_by_diet
+
+## MAKE FIGURES ####
+
+data.plot = ggplot(data, aes(x=Time, y=weight)) +
+  geom_point(aes(color=Diet), position = position_jitter(width = 0.2)) +
+  labs(title="Evolução do peso dos pintinhos nos 21º dias", x="Dias de vida", y="Peso (g)") +
+  theme_classic() +
+  theme(text=element_text(size=18), title=element_text(size=18))
+data.plot
+
+# Write figure to a pdf in the 'figures' folder
+pdf("figures/aumento-peso-pintinhos.pdf")
+# Call plot
+data.plot
+# Close pdf call
+dev.off()
+
+time_21.plot = ggplot(time_21, aes(x=Diet, y=weight)) +
+  geom_boxplot(aes(color=Diet)) +
+  geom_point(aes(color=Diet), position = position_jitter(width = 0.05)) +
+  labs(title="Peso dos pintinhos aos 21 dias de vida", x="Dieta", y="Peso (g)") +
+  theme_classic() +
+  theme(text=element_text(size=18), title=element_text(size=18), legend.position="none")
+time_21.plot
+
+# Write figure to a pdf in the 'figures' folder
+pdf("figures/peso-21dias-pintinhos.pdf")
+# Call plot
+time_21.plot
+# Close pdf call
+dev.off()
+
+weight_time_diet.plot = ggplot(weight_by_time_and_diet, aes(x=Time, y=weight_mean)) +
+  geom_smooth(method = loess, aes(color=Diet), se=FALSE) +
+  geom_point(aes(color=Diet))+
+  labs(title="Evolução do peso dos pintinhos de acordo com a dieta", x="Dias de vida", y="Peso (g)") +
+  theme_classic() +
+  theme(text=element_text(size=18), title=element_text(size=18))
+
+weight_time_diet.plot
+
+
+# Write figure to a pdf in the 'figures' folder
+pdf("figures/media-dietas-pintinhos.pdf")
+# Call plot
+weight_time_diet.plot
+# Close pdf call
+dev.off()
+
