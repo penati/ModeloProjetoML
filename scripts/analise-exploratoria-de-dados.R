3386cb949f9fbb76278a9ef924774260a51f582d
Penati <filipepf15@gmail.com>
2018-06-25 01:48:17 -0300

    Inserção me mais um gráfico com a evolução média das dietas

--- scripts/analise-exploratoria-de-dados.R
+++ scripts/analise-exploratoria-de-dados.R
@@ -38,9 +38,8 @@ tail(time_21)
 ## MAKE FIGURES ####
 
 data.plot = ggplot(data, aes(x=Time, y=weight)) +
-  geom_point(aes(color=Diet)) +
-  geom_smooth(aes(color=Diet), se = FALSE) +
-  labs(title="Aumento de peso do pintinho conforme a dieta", x="Dias de vida", y="Peso (g)") +
+  geom_point(aes(color=Diet), position = position_jitter(width = 0.2)) +
+  labs(title="Evolução do peso dos pintinhos nos 21º dias", x="Dias de vida", y="Peso (g)") +
   theme_classic() +
   theme(text=element_text(size=18), title=element_text(size=18))
 data.plot
@@ -54,7 +53,7 @@ dev.off()
 
 time_21.plot = ggplot(time_21, aes(x=Diet, y=weight)) +
   geom_boxplot(aes(color=Diet)) +
-  geom_point(aes(color=Diet)) +
+  geom_point(aes(color=Diet), position = position_jitter(width = 0.05)) +
   labs(title="Peso dos pintinhos aos 21 dias de vida", x="Dieta", y="Peso (g)") +
   theme_classic() +
   theme(text=element_text(size=18), title=element_text(size=18), legend.position="none")
@@ -67,12 +66,39 @@ time_21.plot
 # Close pdf call
 dev.off()
 
+weight_time_diet.plot = ggplot(weight_by_time_and_diet, aes(x=Time, y=weight_mean)) +
+  geom_smooth(aes(color=Diet), se=FALSE) +
+  geom_point(aes(color=Diet))+
+  labs(title="Evolução do peso dos pintinhos de acordo com a dieta", x="Dias de vida", y="Peso (g)") +
+  theme_classic() +
+  theme(text=element_text(size=18), title=element_text(size=18))
+weight_time_diet.plot
+
+
+# Write figure to a pdf in the 'figures' folder
+pdf("figures/media-dietas-pintinhos.pdf")
+# Call plot
+weight_time_diet.plot
+# Close pdf call
+dev.off()
+
 ## RUN DESCRIPTIVE STATISTICS ####
 ### Summarise data
 
-weight_by_time <- data %>%
+weight_by_time_and_diet <- data %>%
   # agrupando por "Diet" e pelo "Time"
   group_by(Diet, Time) %>%
+  # obtendo a média, desvio padrao, maximo e minimo pesos para cada "Time", de acordo com a dieta
+  summarise(weight_mean = mean(weight),
+            weight_sd = sd(weight),
+            weight_min = min(weight),
+            weight_max = max(weight))
+
+weight_by_time_and_diet
+
+weight_by_time <- data %>%
+  # agrupando por "Diet" e pelo "Time"
+  group_by(Time) %>%
   # obtendo a média, desvio padrao, maximo e minimo pesos para cada "Time", de acordo com a dieta
   summarise(weight_mean = mean(weight),
             weight_sd = sd(weight),
