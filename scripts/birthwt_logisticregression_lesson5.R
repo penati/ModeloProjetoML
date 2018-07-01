4f9d2c6151274221758164f7048e6fabfe71aeb6
Penati <filipepf15@gmail.com>
2018-07-01 01:07:59 -0300

    lesson 5

---
+++ scripts/birthwt_logisticregression_lesson5.R
@@ -0,0 +1,58 @@
+## CARREGANDO PACKAGES ####
+library(rpart)
+library(rpart.plot)
+library(rattle)
+library(RColorBrewer)
+library(readr)
+library(readr)
+library(tidyverse)
+library(Amelia) #visual way to check for missing data
+
+## LEITURA E PREPARACAO DOS DADOS ####
+## Leitura
+
+bw <- read.csv("data/birthwt.csv",stringsAsFactors=FALSE)
+
+## Vizualização dos dados
+str(bw)
+summary(bw)
+hist(bw$bwt)
+
+# convert the categorical variables to factor
+cols <- c('low', 'race', 'smoke', 'ht', 'ui')
+bw[cols] <- lapply(bw[cols], as.factor)
+
+# A visual way to check for missing data
+missmap(bw, main = "Missing values vs observed")
+
+# Split data (training and test groups)
+set.seed(123)
+
+train.index <- sample((nrow(bw)),0.7*nrow(bw))
+
+train <- bw[train.index,]
+test  <- bw[-train.index,]
+
+## Buildind a model ####
+fit <- glm(low ~ age + lwt + race + smoke + ptl + ht + ui + ftv,
+           data = train,
+           family = binomial(link = "logit"))
+summary(fit)
+
+# Using Analysis Of Variance (ANOVA) to analyze the table of deviance
+anova(fit, test="Chisq")
+
+# Check with test set ----
+#newdata needs the same preprocessing as train 
+prediction_prob <- predict(fit,newdata=test,type='response')
+prediction <- ifelse(prediction_prob > 0.5,1,0)
+prop.table(table(prediction,test$low))
+
+## Trying with small model ----
+fit2 <- glm(low ~ lwt + smoke + ptl + ht + ui,
+            data=train, 
+            family=binomial(link="logit"))
+
+prediction_prob2 <- predict(fit2, test, type='response')
+prediction2 <- ifelse(prediction_prob2 > 0.5, 1, 0)
+prop.table(table(prediction2,test$low))
